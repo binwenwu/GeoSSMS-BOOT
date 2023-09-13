d1c18ac4855c8666458800b3b6d3246fffdcdc44
Binwen Wu <2020302131043@whu.edu.cn>
2023-09-13 09:36:29 +0800

    框架初始化

---
+++ src/main/java/com/example/geossms/GeoSsmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.geossms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoSsmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoSsmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/geossms/GeoSsmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geossms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoSsmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
